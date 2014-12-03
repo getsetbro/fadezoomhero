d0a67e8533885dfe273aa75f0b139ae0f1ed7214
Seth Broweleit <hello[@]getsetbro[.]com>
2014-12-03 15:11:20 -0600

    slow burn

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function cycleImages(){
   var $active = $('.active');
   var $next = $active.prev();
   $next.addClass('active');
-  $active.prependTo('.hero').removeClass('active');
+  $active.prependTo('.hero').removeClass('active next');
+  $newactive = $('.active');
+  $newnext = $newactive.prev();
+  $newnext.addClass('next');
 }
 //init
-$('.hero img').last().addClass('active');
+$('.hero img').last().addClass('next');
+$('.hero img').last().prev().addClass('next');
 // run every x seconds
 setInterval(cycleImages, 6000);
