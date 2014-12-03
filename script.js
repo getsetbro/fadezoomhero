1fb82a4beaff7082108f9fcb5add811fb0fe9961
Seth Broweleit <hello[@]getsetbro[.]com>
2014-12-03 09:27:12 -0600

    v2

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+
+
 function cycleImages(){
-  var $active = $('.hero .active');
-  var $next = ($active.next().length > 0) ? $active.next() : $('.hero img:first');
-  $next.css('z-index',2);//move the next image up the pile
-  //$active.animate({"right": "+=100","left": "+=100"}, 5000);
-  $active.fadeOut(5000,function(){//fade out the top image
-    $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-    $next.css('z-index',3).addClass('active');//make the next image the top one
-  });
+  var $active = $('.active');
+  var $next = $active.prev();
+  $next.addClass('active');
+  $active.prependTo('.hero').removeClass('active');
 }
-
-
-  // run every 7s
-  setInterval('cycleImages()', 5000);
+//init
+$('.hero img').last().addClass('active');
+// run every x seconds
+setInterval(cycleImages, 19000);
